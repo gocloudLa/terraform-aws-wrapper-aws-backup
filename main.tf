d58adc95b188d9ad51dd638e4592de612267b716
Federico Midaglia <fmidaglia@gocloud.la>
2025-09-05 12:20:26 -0300

    feat(tags): add custom aditional tags support

--- main.tf
+++ main.tf
@@ -9,6 +9,6 @@ module "aws_backup" {
   kms_key_arn   = try(each.value.force_destroy, var.aws_backup_defaults.force_destroy, null)
   backup_plan   = try(each.value.backup_plan, var.aws_backup_defaults.backup_plan, {})
 
-  tags = local.common_tags
+  tags = merge(local.common_tags, try(each.value.tags, var.aws_backup_defaults.tags, null))
 
 }
